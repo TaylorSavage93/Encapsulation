dc6f07ffde3fe41ee62c6a364ea4e8898ddda644
Taylor Savage <taylorsavage93@gmail.com>
2022-07-14 16:01:13 -0400

    finished exercise

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using BankEncapsulation;
+
+var account = new BankAccount();
+
+Console.WriteLine("Enter amount for deposit.");
+
+var ammountToDeposit = double.Parse(Console.ReadLine());
+
+account.Deposit(ammountToDeposit);
+
+Console.WriteLine($"Thank you for your deposit, your balance is now {account.GetBalance()}.");
+
+
+Console.WriteLine("How much would you like to withdrawl?");
+
+var ammountToWithdrawl = double.Parse(Console.ReadLine());
+
+account.Withdraw(ammountToWithdrawl);
+
+
+Console.WriteLine($"Thank you for your service, your balance is now {account.GetBalance()}.");
